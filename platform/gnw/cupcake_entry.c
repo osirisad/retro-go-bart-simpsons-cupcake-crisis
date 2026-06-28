d552a5cf7c3d6e3c444e8270fb6d2e4efac213b9
osirisad <osirisad@users.noreply.github.com>
2026-06-28 16:58:12 -0400

    fixes

--- platform/gnw/cupcake_entry.c
+++ platform/gnw/cupcake_entry.c
@@ -4,6 +4,7 @@
  */
 #include <stdint.h>
 
+#include "gw_firmware_abi.h"
 #include "main_cupcake.h"
 
 __attribute__((section(".cupcake_entry"), used, noinline))
@@ -13,9 +14,14 @@ void cupcake_overlay_entry(uint8_t load_state, uint8_t start_paused, int8_t save
     extern uint8_t _OVERLAY_CUPCAKE_BSS_END[];
     uint32_t *p = (uint32_t *)_OVERLAY_CUPCAKE_BSS_START;
     uint32_t *e = (uint32_t *)_OVERLAY_CUPCAKE_BSS_END;
+    const gw_firmware_abi_t *abi = gw_firmware_abi();
 
     while (p < e)
         *p++ = 0;
+
+    /* Firmware clears ram_start before dispatch; heap is BSS_END..__RAM_EMU_END__. */
+    if (abi && abi->ram_start_ptr)
+        *abi->ram_start_ptr = (uint32_t)(uintptr_t)_OVERLAY_CUPCAKE_BSS_END;
 
     app_main_cupcake(load_state, start_paused, save_slot);
 }
